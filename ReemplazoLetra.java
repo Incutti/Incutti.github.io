b62806a4ad79c02ff31c76613436dc9cab7d6076
Mateo Incutti <101185808+Incutti@users.noreply.github.com>
2023-03-06 12:01:45 -0300

    Add files via upload

---
+++ ReemplazoLetra.java
@@ -0,0 +1,14 @@
+package unidad0;
+
+public class ReemplazoLetra {
+    public static void main(String[] args) {
+        String frase = "Ayer me compré muñecos de la marca ‘Colchitas’ por internet";
+        char letraIngresada;
+        for(int contador= 0; contador<frase.length();contador++){
+                if(frase.charAt(contador) == 'e'){
+
+            }
+        }
+    }
+}
+
